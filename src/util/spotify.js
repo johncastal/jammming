09675e3ab8d472315248c466bd38a66c07f2542e
johncastal <fredcastal@outlook.com>
2024-01-19 13:45:33 -0500

    update redirectUri

--- src/util/spotify.js
+++ src/util/spotify.js
@@ -1,5 +1,5 @@
 const clientId = '220133310dc04b5cb7049ed1a890d223'; // Insert client ID here.
-const redirectUri = 'https://prismatic-lebkuchen-e86680.netlify.app'; // Have to add this to your accepted Spotify redirect URIs on the Spotify API.
+const redirectUri = 'https://jammmingjcastal.netlify.app'; // Have to add this to your accepted Spotify redirect URIs on the Spotify API.
 let accessToken; 
 
 const Spotify = {
